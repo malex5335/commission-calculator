acd3ac4ea8a9db29a7e92300831fd790801cc983
Maximilian Rich <maximilian.rich@online.de>
2023-04-17 21:44:26 +0200

    remove unused lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-params")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
